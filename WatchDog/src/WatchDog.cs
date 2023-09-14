506767c3eb898d6ed3db146ad2c04354281e4b49
Joe <mr.gulyas.jozsef.ev@gmail.com>
2023-09-14 10:11:37 +0200

    Shorten the value of the req/resp body if too long

--- WatchDog/src/WatchDog.cs
+++ WatchDog/src/WatchDog.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WatchDog.src.Enums;
 using WatchDog.src.Helpers;
 using WatchDog.src.Interfaces;
@@ -86,7 +88,7 @@ namespace WatchDog.src {
                 context.Request.EnableBuffering();
                 await using var requestStream = _recyclableMemoryStreamManager.GetStream();
                 await context.Request.Body.CopyToAsync(requestStream);
-                requestBodyDto.RequestBody = GeneralHelper.ReadStreamInChunks(requestStream);
+                requestBodyDto.RequestBody = shortenBody(GeneralHelper.ReadStreamInChunks(requestStream));
                 context.Request.Body.Position = 0;
             }
             RequestLog = requestBodyDto;
@@ -103,7 +105,7 @@ namespace WatchDog.src {
                         var responseBody = await new StreamReader(context.Response.Body).ReadToEndAsync();
                         context.Response.Body.Seek(0, SeekOrigin.Begin);
                         var responseBodyDto = new ResponseModel {
-                            ResponseBody = responseBody,
+                            ResponseBody = shortenBody(responseBody),
                             ResponseStatus = context.Response.StatusCode,
                             FinishTime = DateTime.Now,
                             Headers = context.Response.Headers.ContentLength > 0 ? context.Response.Headers.Select(x => x.ToString()).Aggregate((a, b) => a + ": " + b) : string.Empty,
@@ -121,6 +123,25 @@ namespace WatchDog.src {
                 } finally {
                     context.Response.Body = originalBodyStream;
                 }
+            }
+        }
+
+        private string shortenBody(string body) {
+            try {
+                JObject jsonObj = JObject.Parse(body);
+
+                foreach (JProperty property in jsonObj.DescendantsAndSelf().OfType<JProperty>()) {
+                    if (property.Value is JValue jValue && jValue.Type == JTokenType.String) {
+                        string originalValue = jValue.Value.ToString();
+                        if (originalValue.Length > 100) {
+                            jValue.Value = $"{originalValue.Substring(0, 97)}...";
+                        }
+                    }
+                }
+
+                return jsonObj.ToString(Formatting.Indented);
+            } catch (JsonReaderException) {
+                return body.Substring(0, 200);
             }
         }
     }
